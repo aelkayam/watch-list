66b36024f212e801d99a114a8c0ca887aa04ecd5
Elkayam Melesse <elka@duck.com>
2022-07-22 21:15:19 +0300

    clean-up server

--- server/server.js
+++ server/server.js
@@ -3,17 +3,9 @@ import { readFile } from "fs";
 import cors from "cors";
 
 const app = express();
-//const port = process.env.PORT || 5000;
 const port = 3001;
 
 app.use(cors());
-// app.use(json());
-// app.use(urlencoded({ extended: true }));
-
-// // API calls
-// app.get("/api/hello", (req, res) => {
-//   res.send({ express: "Hello From Express" });
-// });
 
 app.get("/ghibli-data", (req, res) => {
   readFile("./ghibli-data.json", "utf-8", (err, data) => {
@@ -26,5 +18,3 @@ app.get("/ghibli-data", (req, res) => {
 app.listen(port, () => {
   console.log(`app listening on port ${port}!`);
 });
-
-// app.listen(port, () => console.log(`Listening on port ${port}`));
